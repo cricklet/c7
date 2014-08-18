eb41fc4d68ff6418ad78ef9019ccf21421cbf8d1
Kenrick Rilee <kenrick.rilee@gmail.com>
2014-08-17 21:48:21 -0400

    Finally have piano working...

--- js/piano.js
+++ js/piano.js
@@ -2,73 +2,77 @@ define(function (require) {
   var jsynth = require('lib/jsynth');
   var _      = require('lib/underscore');
 
+  /* A note looks like this:
+   * {
+   *   sustained: true,
+   *   done: true,
+   *   adsr: {...},
+   *   midi: 50,
+   *   freq: midiToFreq(50),
+   *   ...
+   * }
+   */
+
+  function midiToFreq (midi) {
+    return Math.pow(2, (midi - 69.0) / 12.0) * 440;
+  }
 
   /****************************************************************************
   * Helpers for generating sounds.
   * Generators return null when they are finished.
   */
-  function adsrEnvelopeFactory (attack, decay, sustainLevel, release) {
-    var stopTime = -1;
-
-    var attackEnvelope = function (time) {
-      return time / attack;
-    }
-
-    var decayEnvelope = function (time) {
-      return 1 - time * (1 - sustainLevel) / decay;
-    }
-
-    var releaseEnvelope = function (time) {
-      return sustainLevel * (1 - time / release);
-    }
-
-    return function (time, sustained) {
-      if (time < attack) return attackEnvelope(time);
-      time -= attack;
-
-      if (time < decay) return decayEnvelope(time);
-      time -= decay;
-
-      if (sustained) {
-        return sustainLevel;
-
-      } else {
-        if (stopTime == -1) stopTime = time;
-        time -= stopTime;
-
-        if (time < release) return releaseEnvelope(time)
-      }
-
-      return null
-    }
+  function attackEnvelope (time, attack) {
+    return time / attack;
   }
 
-  function sineGeneratorFactory (freq) {
-    return function (time) {
-      return Math.sin(time * 2 * Math.PI * freq)
-    };
+  function decayEnvelope (time, sustain, decay) {
+    return 1 - time * (1 - sustain) / decay;
+  }
+
+  function releaseEnvelope (time, sustain, release) {
+    return sustain * (1 - time / release);
+  }
+
+  function adsrEnvelope (t, note) {
+    var sustained = note.sustained;
+
+    var opts    = note.adsr;
+    var attack  = opts.attack;
+    var decay   = opts.decay;
+    var sustain = opts.sustain;
+    var release = opts.release;
+
+    if (t < attack) return attackEnvelope(t, attack);
+    t -= attack;
+
+    if (t < decay) return decayEnvelope(t, sustain, decay);
+    t -= decay;
+
+    if (sustained) return sustain;
+
+    if (opts._stop === undefined) opts._stop = t;
+    t -= opts._stop;
+
+    if (t < release) return releaseEnvelope(t, sustain, release);
+
+    return null
+  }
+
+  function sineGenerator (time, note) {
+    return Math.sin(time * 2 * Math.PI * note.freq)
   }
 
   /****************************************************************************
   * Plays a single note.
   */
-  function midiToFreq (midi) {
-    return Math.pow(2, (midi - 69.0) / 12.0) * 440;
-  }
+  function generate (time, note) {
+    var effect = adsrEnvelope(time, note);
+    var signal = sineGenerator(time, note);
 
-  function generatorFactory (m,a,d,s,r) {
-    var f = midiToFreq(m);
-    adsrEnv = adsrEnvelopeFactory(a,d,s,r);
-    sineGen = sineGeneratorFactory(f);
-    return function (t, sustained) {
-      var sine = sineGen(t);
-      var adsr = adsrEnv(t, sustained);
-
-      if (adsr === null || sine === null) {
-        return null;
-      } else {
-        return adsr * sine;
-      }
+    if (effect === null || signal === null) {
+      return null;
+    } else {
+      return effect * signal;
     }
   }
 
@@ -76,23 +80,75 @@ define(function (require) {
   * Controller
   */
   var Piano = function (context, output) {
-    var gen = generatorFactory(midiToFreq(40), 0.2, 0.4, 0.8, 0.2)
-
-    synth = jsynth(context, function (t) {
-      var val = gen(t, true);
-      return val;
-    });
+    synth = jsynth(context, this.generate.bind(this));
     synth.connect(output)
 
+    this.notes = [];
+    setInterval(this.cleanNotes.bind(this), 500);
   }
 
-  Piano.prototype.allNotesUp = function (except) {
+  Piano.prototype.generate = function (t) {
+    var overall = 0;
+    for (var i in this.notes) {
+      var note = this.notes[i];
+
+      if (note.done) continue;
+
+      if (note._start === undefined) note._start = t;
+
+      var val = generate(t - note._start, note);
+
+      if (val === null) {
+        note.done = true;
+      } else {
+        overall += val;
+      }
+    }
+
+    return overall;
+  }
+
+  Piano.prototype.cleanNotes = function () {
+    this.notes = _.filter(this.notes, function (note) {
+      return note.done !== true;
+    });
+  }
+
+  Piano.prototype.allNotesUp = function (exceptMidi) {
+    for (var i in this.notes) {
+      var note = this.notes[i];
+      if (note.midi !== exceptMidi) note.sustained = false;
+    }
   }
 
   Piano.prototype.noteUp = function (midi) {
+    for (var i in this.notes) {
+      var note = this.notes[i];
+      if (note.midi === midi) note.sustained = false;
+    }
   }
 
   Piano.prototype.noteDown = function (midi) {
+    var alreadyDown = _.some(this.notes, function (note) {
+      return note.midi === midi && note.sustained;
+    });
+
+    if (alreadyDown) return;
+
+    var note = {
+      midi: midi,
+      freq: midiToFreq(midi),
+      sustained: true,
+      done: false,
+      adsr: {
+        attack:  0.2,
+        decay:   0.4,
+        sustain: 0.5,
+        release: 0.4
+      }
+    };
+
+    this.notes.push(note);
   }
 
   return Piano;
